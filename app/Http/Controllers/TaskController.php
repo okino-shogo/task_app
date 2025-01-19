cc2437be90475cec3df39bf8b316ef2a30b3a750
okino-shogo <okisho0106@gmail.com>
2025-01-19 21:01:17 +0900

    照会機能作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
-    //
+    // indexページへ移動
+    public function index()
+    {
+        // モデル名::テーブル全件取得
+        $tasks = Task::all();
+
+        return view('tasks.index', ['tasks' => $tasks]);
+    }
+
+    public function show($id)
+    {
+        $task = Task::find($id);
+        return view('tasks.show', ['task' => $task]);
+    }
 }
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>タスク一覧表示</title>
+</head>
+<body>
+    <h1>タスク一覧</h1>
+    <ul>
+        @foreach ($tasks as $task)
+            <!-- // リンク先をidで取得し名前で出力 -->
+            <li><a href="{{ route('tasks.show', $task) }}">{{ $task->title }}</a></li>
+        @endforeach
+    </ul>
+</body>
+</html>
